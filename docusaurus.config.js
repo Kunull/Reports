c3cd08c256c264d67a2bca6800d1b44ad1f8ce1b
Kunal Walavalkar <kunalw2002@gmail.com>
2024-07-13 11:52:53 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Reports',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/null_medium.png',
+  favicon: 'img/0_medium.png',
 
   // Set the production url of your site here
   url: 'https://github.com',
